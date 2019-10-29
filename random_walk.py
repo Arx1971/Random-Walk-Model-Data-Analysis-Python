fca2ed651b156b98a4e028d9f68f39b2350b7463
Arx1971 <adnan.rahin13@gmail.com>
2019-10-28 21:43:14 -0400

    update

--- random_walk.py
+++ random_walk.py
@@ -6,7 +6,7 @@ class RandomWalk():
     def __init__(self, num_points=500):
         self.num_points = num_points
         self.x_values = [0]
-        self.y.values = [0]
+        self.y_values = [0]
 
     def fill_walk(self):
         while len(self.x_values) < self.num_points:
